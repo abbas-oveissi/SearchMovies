d739d2a4cac3f550ea6fd0a9ddde29ef36d7bd4d
Abbas Oveissi <abbas.oveissi@gmail.com>
2016-05-26 20:44:04 +0430

    dorost kardan e searchview

---
+++ app/src/main/java/ir/oveissi/searchmovies/customview/SearchView.java
@@ -0,0 +1,106 @@
+package ir.oveissi.searchmovies.customview;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import ir.oveissi.searchmovies.R;
+
+public class SearchView extends RelativeLayout {
+
+    public Context mContext;
+    ImageButton imClearTextBtn;
+    EditText etTerms;
+    performSearchListener listener;
+
+
+    public SearchView(Context context) {
+        super(context);
+        initializeViews(context,null);
+    }
+
+    public SearchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeViews(context,attrs);
+    }
+    public SearchView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initializeViews(context,attrs);
+    }
+
+    public void setListener(performSearchListener listener) {
+        this.listener = listener;
+    }
+
+    private void initializeViews(Context context,AttributeSet attrs) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.view_search, this);
+        this.mContext=context;
+        imClearTextBtn=(ImageButton)findViewById(R.id.imClearTextBtn);
+        etTerms=(EditText)findViewById(R.id.etTerms);
+
+
+        etTerms.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    if(listener!=null)
+                        listener.performSearch(etTerms.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        etTerms.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                imClearTextBtn.setVisibility(View.VISIBLE);
+            }
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0) {
+                    imClearTextBtn.setVisibility(View.GONE);
+                } else{
+                    imClearTextBtn.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        imClearTextBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etTerms.setText("");
+            }
+        });
+
+
+
+    }
+
+
+    public String getValue()
+    {
+        return etTerms.getText().toString();
+    }
+
+
+    public void setText(String text)
+    {
+        etTerms.setText(text);
+    }
+    public interface performSearchListener
+    {
+        public void performSearch(String terms);
+    }
+    
+}
